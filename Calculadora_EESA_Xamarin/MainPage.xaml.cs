29cde2bb060a746a9a060828c45f38fb1a8e8e04
CarlosArmando037 <elramirosalas400@gmail.com>
2023-10-05 12:29:24 -0700

    5.0

--- Calculadora_EESA_Xamarin/MainPage.xaml.cs
+++ Calculadora_EESA_Xamarin/MainPage.xaml.cs
@@ -10,17 +10,69 @@ namespace Calculadora_EESA_Xamarin
 {
     public partial class MainPage : ContentPage
     {
+        string entrada;
+        double resultado = 0;
+        string operador = "";
+
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void ()
+        private void Numero(object sender, EventArgs e)
         {
-private void uno_Clicked(object sender, EventArgs e)
-        {
+            Button boton = (Button)sender;
+            string textoBoton = boton.Text;
 
+            if (textoBoton == "AC")
+            {
+                entrada = "";
+                resultado = 0;
+                operador = "";
+            }
+            else if (textoBoton == "C")
+            {
+                entrada = "";
+            }
+            else if (textoBoton == "=")
+            {
+                if (!string.IsNullOrEmpty(entrada))
+                {
+                    double numeroActual = double.Parse(entrada);
+                    switch (operador)
+                    {
+                        case "+":
+                            resultado += numeroActual;
+                            break;
+                        case "-":
+                            resultado -= numeroActual;
+                            break;
+                        case "*":
+                            resultado *= numeroActual;
+                            break;
+                        case "/":
+                            if (numeroActual != 0)
+                                resultado /= numeroActual;
+                            else
+                                Resultado.Text = "Error";
+                            break;
+                        case "%":
+                            resultado %= numeroActual;
+                            break;
+                        default:
+                            resultado = numeroActual;
+                            break;
+                    }
+                    entrada = resultado.ToString();
+                    operador = "";
+                }
+            }
+            else
+            {
+                entrada += textoBoton;
+            }
+
+            Resultado.Text = entrada;
         }
-    }
     }
 }
